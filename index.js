5d38f6a414afe07315dd1604d3b2b35ca2209852
cesarsld <jaimes.cesar88@gmail.com>
2019-03-10 21:13:42 +0000

    Added sale API

--- index.js
+++ index.js
@@ -20,10 +20,78 @@ mongoClient.connect(function (err) {
     assert.equal(null, err);
     console.log("connected successfully to server");
 });
+app.get('/api/saleData', function (req, res) {
+    var unix = Math.floor(new Date() / 1000);
+    var queryLength = 0;
+    if (req.query.dayNeeded) {
+        var number = parseInt(req.query.dayNeeded);
+        if (number > 0)
+            queryLength = number + 2;
+        else
+            queryLength = Math.floor(unix / 86400);
+    }
+    else
+        unix = 0;
+    const db = mongoClient.db('AxieData');
+    var dbName = '';
+    if (req.query.type) {
+        switch (req.query.type) {
+            case 'Tag':
+                if (req.query.tag) {
+                    switch (req.query.tag) {
+                        case 'Agamo':
+                            dbName = 'Agamogenesis';
+                            break;
+                        case 'MEO':
+                            dbName = 'MEO';
+                            break;
+                        case 'MEO2':
+                            dbName = 'MEO2';
+                            break;
+                        case 'Origin':
+                            dbName = 'Origin';
+                            break;
+                        case 'NoTag':
+                            dbName = 'Untagged';
+                            break;
+                    }
+                }
+                break;
+            case 'Mystic':
+                if (req.query.mysticCount) {
+                    switch (req.query.mysticCount) {
+                        case '1':
+                            dbName = '1';
+                            break;
+                        case '2':
+                            dbName = '2';
+                            break;
+                        case '3':
+                            dbName = '3';
+                            break;
+                        case '4':
+                            dbName = '4';
+                            break;
+                        case 'any':
+                            dbName = 'AnyMystic';
+                            break;
+                    }
+                }
+                break;
+        }
+    }
+    var collec = db.collection(dbName);
+    collec.find({ _id: { $gt: unix - 86400 * queryLength } }).toArray(function (err, result) {
+        if (err) throw err;
+        if (queryLength !== 0)
+            while (result.length > queryLength - 2)
+                result.shift();
+        res.json(result);
 
+    });
+});
 app.get('/api/dailyDau', function (req, res) {
     console.log('dau query!');
-
     const db = mongoClient.db('AxieData');
     var collec = db.collection('DailyBattleDAU');
     collec.find({}).toArray(function (err, result) {
@@ -104,6 +172,6 @@ app.get('*', (req, res) => {
 //    }); 
 //}
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log("Server started. Listening on port " + port);
 });
